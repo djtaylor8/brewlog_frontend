49827048583c1d35cca164b854e2d45b699dbd36
DJ Taylor <dtaylor88@gmail.com>
2021-09-04 17:11:40 -0700

    adjust map on page

--- src/index.js
+++ src/index.js
@@ -3,4 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch(endPoint) 
     .then(res => res.json())
     .then(json => console.log(json));
+   
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZGp0YXlsb3IiLCJhIjoiY2t0NmJyNTh3MGd6aTJxbzhzajV5d251OSJ9.i0hGcpcDK2kDXEyK3l2NHA'
+    
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-118.24335241609123,34.0524555209941],
+        zoom: 13
+    });
+ 
+        // Add the control to the map.
+        map.addControl(
+            new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            mapboxgl: mapboxgl
+        })
+    );
+
 });
